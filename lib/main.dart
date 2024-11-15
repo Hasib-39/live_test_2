8ea509cbfa5c948adae2bd248c436f1d4d17e3b2
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-11-15 22:20:54 +0600

    Logo and CircleAvatar added

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,54 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DonateBlood(),
+    );
   }
 }
+
+class DonateBlood extends StatelessWidget {
+  const DonateBlood({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            "Need Blood",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+              onPressed: (){},
+              icon: const Icon(
+                  Icons.add,
+                color: Colors.white,
+              )
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.grey[600],
+              radius: 90,
+              child: const Icon(
+                Icons.bloodtype_outlined,
+                color: Colors.red,
+                size:100,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
